581ef7ae70ee2c037ae99ac26b42e040407fd481
Aayush Saklani <57747255+THUNDER-BIT@users.noreply.github.com>
2022-07-04 01:20:04 +0530

    Update test_scan.py
    Added displaying of packet information(Flags)

--- test_scan.py
+++ test_scan.py
@@ -2,44 +2,59 @@ import socket
 import struct
 import textwrap
 
-
-# main program is a big infinite loop that will keep on listening to packets
+# The main program is a big infinite loop that will keep on listening to packets
+#sockets are a type of stateful connections and they do not terminate the connection 
 def main():
     connect = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    
+
 
     while True:
-        raw_data, addr = connect.recvfrom(65536)
+        raw_data, addr = connect.recvfrom(65530)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print("\nEthernet Frame")
         print("Destination: {}, Source: {} , Protocol: {}".format(dest_mac, src_mac, eth_proto))
 
-        if eth_proto == 8:  # 8 is for IPv4 normal intenet traffic
+        if eth_proto == 8:  # 8 bytes is for IPv4 add.
             version, header_length, ttl, proto, src, target, data = IPv4_packet(data)
-            print("IPv4 Packet")
+            print("IPv4 Packet: ")  # Prints Header info for IPv4 packet
             print("Version: {}, Header Length: {}, TTL: {}".format(version, header_length, ttl))
-            print("Protocal: {}, source: {}, Target: {}".format(proto, src, target))
+            print("Protocol: {}, source: {}, Target: {}".format(proto, src, target))
 
-            if proto == 1:
+            if proto == 1:  # 1 is for ICMP packet
                 icmp_type, code, checksum, data = icmp_packet(data)
-                print("ICMP packet:")
+                print("ICMP packet: ")
+                print("Type: {} , Code: {}, Checksum: {}".format(icmp_type, code, checksum))
+                print("Data: ")
+                print(format_multi_line('\t\t\t', data))
 
+            elif proto == 6:  # 6 is for TCP data
+                src_port, dest_port, seq, ack, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data = tcp_segment(data)
+                print("TCP Packet: ")
+                print("Sorce Port: {} Destination Port: {}".format(src_port, dest_port))
+                print("Sequence: {}, Acknowledgement: {},".format(seq, ack))
+                print("FLAGS: ")
+                print("URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}".format(flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin))
+                print("DATA: ")
+                print(format_multi_line('\t\t\t', data))
 
-            elif proto == 6:
-                print("TCP Packet:")
+            elif proto == 17:  # 17 is for udp segment
+                src_port, dest_port, size, data = udp_segment(data)
+                print("UDP Segment: ")
+                print("Source Post: {}, Destination Port: {}, Length: {}".format(src_port, dest_port, size))
 
-            elif proto == 17:
-                print("UDP Segment")
-
+                # Other
             else:
                 print("Data:")
+                print(format_multi_line('\t\t', data))
         else:
             print("Data:")
+            print(format_multi_line('\t', data))
 
 
 # unpack ethernet frame
 def ethernet_frame(data):
-    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
-    # the ! is for specifying that data os network big indian n little indian
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])		# the ! is for specifying that data os network format
     return get_mac_addr(src_mac), get_mac_addr(dest_mac), socket.htons(proto), data[14:]
 
 
